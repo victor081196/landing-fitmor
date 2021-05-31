4577174b0accdfbc1d1752d33830cad1bab1d5d4
yovanny97 <54336590+yovanny97@users.noreply.github.com>
2021-05-31 11:59:54 -0500

    Update correo.php

--- correo.php
+++ correo.php
@@ -19,8 +19,8 @@ if (isset($_POST)) {
     $mail->isSMTP();                                            //Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'yova007.sanchez@gmail.com';                     //SMTP username
-    $mail->Password   = 'qwerty123yo';                               //SMTP password
+    $mail->Username   = '';                     //SMTP username
+    $mail->Password   = '';                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
     $mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
